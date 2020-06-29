06ba5f586106b88d6c815af4c72adce1f6ecef27
George Boots <ggboots.99@gmail.com>
2020-06-29 03:16:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
-
-var animals = ["dog", "cat", "squirrel"];
-

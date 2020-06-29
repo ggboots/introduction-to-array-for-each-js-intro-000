87ca7f30510321a2bd6a7e24748b1c7c0cd03fea
George Boots <ggboots.99@gmail.com>
2020-06-29 03:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function doToElementsInArray(){
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
+
+var animals = ["dog", "cat", "squirrel"];
+
+doToElementsInArray(animals, changeCompletely);
+
+console.log(animals);

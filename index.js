4a8143462ae324677fb25368b7a5a3eb49bf13de
George Boots <ggboots.99@gmail.com>
2020-06-29 03:10:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add your doToElementsInArray() function here:
 
-function doToElementsInArray(){
-
+function doToElementsInArray(array, callback){
+  array.forEach(callback)
 }
 // Add your changeCompletely() function here:
 function changeCompletely() {

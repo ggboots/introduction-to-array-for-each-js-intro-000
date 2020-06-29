993302deb1d7fcecdb270bde4e204276a9156b19
George Boots <ggboots.99@gmail.com>
2020-06-29 03:15:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ function changeCompletely(element, index, array) {
 
 var animals = ["dog", "cat", "squirrel"];
 
-doToElementsInArray(animals, changeCompletely);
-
-// log out animals -- pretty cool, right?
-console.log(animals);
